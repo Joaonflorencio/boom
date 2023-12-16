9b7375e4fa4e7f8c8dc5312d8479a6527de68fa6
João Neto <jflorencioadv@gmail.com>
2023-12-16 15:19:08 +0100

    boom

--- js/script.js
+++ js/script.js
@@ -1,50 +1,105 @@
+let numberSelector = 0
 
-let gameNumber;
-let userNumber;
-let countdown = 5;
-let countdownInterval;
-    
-document.addEventListener('keydown', function(event) {
-    if(event.key === 'Enter'){
-        const Game = new Promise((resolve) => {
-            countdownInterval = setInterval(() => {
-                document.getElementById('countdown').innerHTML = `Countdown: ${countdown} seconds`;
-                countdown--;
-                if(countdown < 0) {
-                    clearInterval(countdownInterval)
-                    resolve();
-                }
-            }, 1000);
-        });
+const userInput = document.getElementById('userInput'),
+      countDown = document.getElementById('countdown'),
+      result    = document.getElementById('result'),
+      restart   = document.getElementById('restart')
 
-        userNumber = parseInt(document.getElementById('userInput').value, 10);
-
-        function displayResult () {
-            if(userNumber === gameNumber) {
-                document.getElementById('result').innerHTML = `
-                    <div id='result-green'>Has salvado el mundo!</div> 
-                    <div class ='message'>Has elegido el número ${userNumber} y el número correcto era ${gameNumber}.</div>
-                `
-            } else {
-                document.getElementById('result').innerHTML = `
-                    <div id='result-red'>La bomba ha estallado!</div> 
-                    <div class ='message'>Has elegido el número ${userNumber} y el número correcto era ${gameNumber}.</div>
-                `
-            }
-        }
-
-        Game.then(() => {
-            gameNumber = Math.floor(Math.random() * 3) + 1;
-            displayResult();
-            console.log(gameNumber)
-        })
-    }
-});
-    
-document.getElementById('restart').addEventListener('click', function restartGame(){
-    clearInterval(countdownInterval);
-    document.getElementById('countdown').innerHTML = '';
-    document.getElementById('result').innerHTML = '';
-    document.getElementById('userInput').value = '';
-    countdown = 5;
+userInput.addEventListener('change', function() {
+  numberSelector = userInput.value
 })
+
+function startGame() {
+  startCountdown(5)
+  const mysteryNumber = new Promise((resolve) => {
+    setTimeout(() => {
+      const randomNum = Math.floor(Math.random() * 3) + 1;
+      resolve(randomNum)
+      // return mysteryNumber
+    }, 6000); //set interval son 6 vueltas (del 5 al 0)
+  }) 
+  return mysteryNumber
+}
+
+function startCountdown(count) {
+  setInterval(() => {
+    if (count >= 0) {
+      countDown.innerHTML = `Cuenta atrás: ${count} segundos`;
+      count--;
+    } else {
+      // clearInterval(countdownInterval);
+      // countDown.innerHTML = '';
+    }
+  }, 1000);
+}
+  
+startGame().then(number => {
+  mensaje = ''
+  if (number == numberSelector) {
+    mensaje = `
+    <span class="green">Enhorabuena, has salvado el mundo 👑</span>
+    <p>Tu número ${numberSelector} es el mismo que el número ${number}</p>`
+  } else {
+    mensaje = `
+    <span class="red">La bomba 💣💥 ha estallado</span>
+    <p>Tu número ${numberSelector} no es el mismo que el número ${number}</p>
+    `
+  }
+  result.innerHTML = mensaje
+})
+
+restart.addEventListener('click', function() {
+  location.reload()
+})
+
+
+
+
+
+
+
+
+
+
+// function startGame() {
+//   startCountdown(5)
+//     .then((randomNum) => {
+//       const userInput = document.getElementById('userInput').value;
+
+//       if (userInput === '') {
+//         document.getElementById('result').innerHTML = 'La bomba ha estallado. No has escrito nada. 💣';
+//       } else if (userInput == randomNum) {
+//         document.getElementById('result').innerHTML = '¡Has salvado el mundo!';
+//       } else {
+//         document.getElementById('result').innerHTML = `La bomba ha estallado. El número era: ${randomNum} 💣`;
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+// function startCountdown(seconds) {
+//   return new Promise((resolve, reject) => {
+//     let count = seconds;
+//     const countdownElement = document.getElementById('countdown');
+
+//     const countdownInterval = setInterval(() => {
+//       if (count > 0) {
+//         countdownElement.innerHTML = `Cuenta atrás: ${count} segundos`;
+//         count--;
+//       } else {
+//         clearInterval(countdownInterval);
+//         countdownElement.innerHTML = '';
+//         const randomNum = Math.floor(Math.random() * 3) + 1;
+//         resolve(randomNum);
+//       }
+//     }, 1000);
+//   });
+// }
+
+// function restartGame() {
+//   document.getElementById('userInput').value = '';
+//   document.getElementById('result').innerHTML = '';
+//   startGame();
+//}
